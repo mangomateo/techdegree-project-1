ded1989920a81788231ad4c3d0a63dfbcce33e24
mangomateo <velarde.matthew@gmail.com>
2021-01-27 15:45:50 -0800

    added object to quotes array

--- js/script.js
+++ js/script.js
@@ -15,43 +15,57 @@ let quotes = [
     quote: `And so, my fellow Americans: ask not what your country can do for you -- ask what you can do for your country.`,
     source: `John F. Kennedy`,
     citation: `Inauguration Address`,
-    year: '1961'
+    year: '1961',
+    tags: ['history', 'politics']
   },
   {
     quote: `The dream was always running ahead of me. To catch up, to live for a moment in unison with it, that was the miracle.`,
     source: `Anais Nin`,
     citation: undefined,
-    year: undefined
+    year: undefined,
+    tags: ['literature', 'poetry']
   },
   {
     quote: `Tough times never last, but tough people do.`,
     source: `Robert H. Schuller`,
     citation: undefined,
-    year: undefined
+    year: undefined,
+    tags: ['motivation', 'religion']
   },
   {
     quote: `We talkin' about practice?`,
     source: `Allen Iverson`,
     citation: undefined,
-    year: `2002`
+    year: `2002`,
+    tags: ['sports', 'basketball']
   },
   {
     quote: `I can't believe what you say because I can see what you do.`,
     source: `James Baldwin`,
     citation: undefined,
-    year: undefined
+    year: undefined,
+    tags: ['social justice', 'civil rights']
   },
   {
     quote: 'Just keep swimming!',
-    source: `Anais Nin`,
+    source: `Dory`,
     citation: `Finding Nemo`,
-    year: `2003`
+    year: `2003`,
+    tags: ['disney', 'film']
   },
   {
     quote: `To infinity and beyond!`,
     source: `Buzz Lightyear`,
     citation: `Toy Story`,
-    year: `1995`
+    year: `1995`,
+    tags: ['pixar', 'film']
+  },
+  {
+    quote: `Do not be dismayed by the brokenness of the world. All things break. And all things can be mended. Not with time, as they say, but with intention. SO go. Love intentionally, extravagantly, unconditionally. The broken world waits in darkness for the light that is you.`,
+    source: `L. R. Knost`,
+    citation: undefined,
+    year: undefined,
+    tags: ['motivation', 'healing']
   }
 ];
 
@@ -73,4 +87,4 @@ let quotes = [
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+// document.getElementById('load-quote').addEventListener("click", printQuote, false);
