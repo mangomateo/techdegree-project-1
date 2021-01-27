4cee64263b988da2446b7a01efd9b102bd79850d
mangomateo <velarde.matthew@gmail.com>
2021-01-27 11:15:43 -0800

    created quotes array

--- js/script.js
+++ js/script.js
@@ -3,14 +3,57 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
+// For assistance:
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
+/***
+ * `quotes` array
 ***/
-
+let quotes = [
+  {
+    quote: `And so, my fellow Americans: ask not what your country can do for you -- ask what you can do for your country.`,
+    source: `John F. Kennedy`,
+    citation: `Inauguration Address`,
+    year: '1961'
+  },
+  {
+    quote: `The dream was always running ahead of me. To catch up, to live for a moment in unison with it, that was the miracle.`,
+    source: `Anais Nin`,
+    citation: undefined,
+    year: undefined
+  },
+  {
+    quote: `Tough times never last, but tough people do.`,
+    source: `Robert H. Schuller`,
+    citation: undefined,
+    year: undefined
+  },
+  {
+    quote: `We talkin' about practice?`,
+    source: `Allen Iverson`,
+    citation: undefined,
+    year: `2002`
+  },
+  {
+    quote: `I can't believe what you say because I can see what you do.`,
+    source: `James Baldwin`,
+    citation: undefined,
+    year: undefined
+  },
+  {
+    quote: 'Just keep swimming!',
+    source: `Anais Nin`,
+    citation: `Finding Nemo`,
+    year: `2003`
+  },
+  {
+    quote: `To infinity and beyond!`,
+    source: `Buzz Lightyear`,
+    citation: `Toy Story`,
+    year: `1995`
+  }
+];
 
 
 /***
